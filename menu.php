babd24603d556b674de35b9a013dc06556ec7154
ZyphonGT <rickygani10@gmail.com>
2019-03-14 15:11:38 +0700

    Added Session Expiry using PHP

--- menu.php
+++ menu.php
@@ -24,6 +24,18 @@ if(!isset($_SESSION['uid'])){
 } else{
 	$username = $_SESSION['uid'];
 }
+
+
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 10)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time 
+    session_destroy();   // destroy session data in storage
+    header("Location: login.php?error=inactivity");
+    exit();
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+
 ?>
 
     <div class="container my-4">
