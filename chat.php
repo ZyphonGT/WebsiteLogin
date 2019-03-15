d970bc334914893160e660463755277f068a0673
ZyphonGT <rickygani10@gmail.com>
2019-03-16 01:56:37 +0700

    Learned to fetch SQL query result to PHP array

--- chat.php
+++ chat.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+$link = mysqli_connect("localhost","root","","brinets");
+    if(!$link) {
+        die("Connection Failed: ".mysqli_connect_error());
+    }
 /******************
  * Take Username  *
  ******************/
@@ -30,6 +34,44 @@ require 'header.php';
 
 <main>
     <h1>New Page</h1>
+    <?php
+        $sql  = "SELECT username, email FROM user";
+        $stmt = mysqli_stmt_init($link);
+        if(!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: chat.php?error=sqlerror1");
+            exit();
+        } else {
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            $datas = array();
+
+
+            //If username is found
+            if(mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+                    array_push($datas, $row);
+                }
+
+                print_r($datas);
+
+                foreach($datas as $line) {
+                    // print_r($uid." ");
+                    foreach($line as $x)
+                    {
+                        print_r($x."<br>");
+                    }    
+                }
+                print_r(count($datas)."<br>");
+            } 
+            else {
+                //username NOT found
+                header("Location: login.php?error=uidNotFound&uid=".$username);
+                exit();
+            }
+
+        }
+    ?>
 </main>
 
 <?php
