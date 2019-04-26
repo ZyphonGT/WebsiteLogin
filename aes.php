8b8f77af35af0c0adda5c4c90e307c05e401d7d0
Mailman <albertantonio88@gmail.com>
2019-04-26 12:45:35 +0700

    aes

--- aes.php
+++ aes.php
@@ -1,48 +1,9 @@
-    <?php
+<?php
         include 'header2.php';
         ?>
         <link rel="stylesheet" href="style/rsa.css">
 
         <main>
-            <div class="menu-bg">
-                <div class="menu-caption">
-                    <div class="shadow">
-                        <h1 class="display-4">
-
-                            <!-- middle text -->Step #1: Generate Private and Public Keys
-                        </h1>
-                        <h6 class="display-4 caption-end">
-                            
-                        </h6>
-                    </div>
-
-                    <form method="POST" action="" class="text-center group-form">
-                        <span class="variable-span" style="left:0px; !important">P : <input type="text" class="small-form" name="pvalue" id="pvalue" aria-describedby="helpId" placeholder="P Value"></span>
-                        <span class="variable-span">Q : <input type="text" class="small-form" name="qvalue" id="qvalue" aria-describedby="helpId" placeholder="Q Value"></span>
-                        <button class="btn btn-outline-warning menu-button" style="margin-top:25px;"><h4 class="display-4 btn-menu-text">
-                        Start Now
-                        </h4></button>
-                    </form>
-                    <div class="text-center">
-                        <span class="variable-span">N : <span class="variable-value">12</span></span><br>
-                        <span class="variable-span">L : <span class="variable-value">13</span></span><br>
-                        <span class="variable-span">E : <span class="variable-value">14</span></span><br>
-                        <span class="variable-span">D : <span class="variable-value">15</span></span><br>
-                    </div>
-                    <div class="text-center">
-                    <span class="variable-span">Private Key(E,N): <input type="text" class="small-form" name="publicKey" id="publicKey" aria-describedby="helpId" placeholder=""></span>
-                    </div>
-
-                    <div class="text-center">
-                    <span class="variable-span">Public Key(D,N): <input type="text" class="small-form" name="privateKey" id="privateKey" aria-describedby="helpId" placeholder=""></span>
-                    </div>
-
-                    </a>
-                </div>
-                
-                
-                
-            </div>
 
             <!-- Step 2 -->
             <div class="menu-bg">
@@ -50,7 +11,7 @@
                     <div class="shadow">
                         <h1 class="display-4">
 
-                            <!-- middle text -->Step #2: Encrypt A Message!
+                            <!-- middle text -->Step #1: Encrypt A Message!
                         </h1>
                         <h6 class="display-4 caption-end">
                             
@@ -58,7 +19,8 @@
                     </div>
 
                     <form method="POST" action="" class="text-center">
-                        <span class="variable-span"><input type="text" class="small-form" name="encryptMsg" id="encryptMsg" style="width:30%;text-align:center;" aria-describedby="helpId" placeholder="Text here"></span>
+                        <span class="variable-span">Key : <input type="text" class="small-form" name="keyvalue" id="keyvalue" aria-describedby="helpId" placeholder=""></span><br>
+                        <span class="variable-span"><input type="text" class="small-form" style="width:30%;text-align:center;margin-top: 10px;" name="encryptMsg" id="encryptMsg" aria-describedby="helpId" placeholder="Text here"></span>
                         <button class="btn btn-outline-warning menu-button"><h4 class="display-4 btn-menu-text">
                         Encrypt Now!
                         </h4></button>
@@ -84,7 +46,7 @@
                     <div class="shadow">
                         <h1 class="display-4">
 
-                            <!-- middle text -->Step #3: Decrypt A Message!
+                            <!-- middle text -->Step #2: Decrypt A Message!
                         </h1>
                         <h6 class="display-4 caption-end">
                             
@@ -92,7 +54,8 @@
                     </div>
 
                     <form method="POST" action="" class="text-center">
-                        <span class="variable-span"><input type="text" class="small-form" style="width:30%;text-align:center;" name="encryptMsg" id="encryptMsg" aria-describedby="helpId" placeholder="*whisper mumble mumble*!"></span>
+                    <span class="variable-span">Key : <input type="text" class="small-form" name="keyvalue" id="keyvalue" aria-describedby="helpId" placeholder=""></span><br>
+                        <span class="variable-span"><input type="text" class="small-form" style="width:30%;text-align:center;margin-top: 10px;" name="encryptMsg" id="encryptMsg" aria-describedby="helpId" placeholder="*whisper mumble mumble*!"></span>
                         <button class="btn btn-outline-warning menu-button"><h4 class="display-4 btn-menu-text">
                         Decrypt Now!
                         </h4></button>
